18aa639453626212055a8872e982c3ab6d4ccf08
Ata-92 <atakal92@gmail.com>
2021-07-05 01:32:59 +0300

    Add Product table and add one-to-many relationship

--- store/models.py
+++ store/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.db.models.fields import DateField, DecimalField
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    createdDate = models.DateField(auto_now_add=True)
+    updatedDate = models.DateField(auto_now=True)
